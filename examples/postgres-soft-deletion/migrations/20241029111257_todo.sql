983215ceb8e41c5f22e63f4dabea4ce29151f5e3
Matt Delacour <MattDelac@users.noreply.github.com>
2025-01-20 14:27:44 -0500

    fix: Fix running two PG examples in CI

--- examples/postgres-soft-deletion/migrations/20241029111257_todo.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS todo;
-CREATE TABLE IF NOT EXISTS todo (
-    id          uuid PRIMARY KEY             NOT NULL DEFAULT gen_random_uuid(),
-    created_at  timestamp with time zone     NOT NULL DEFAULT (now() at time zone 'utc'),
-    updated_at  timestamp with time zone     NOT NULL DEFAULT (now() at time zone 'utc'),
-    deleted_at  timestamp with time zone     NULL,
-    description TEXT                         NOT NULL,
-    done        BOOLEAN                      NOT NULL DEFAULT FALSE
-);
